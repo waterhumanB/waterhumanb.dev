1db1a1e0a17d2020587bbc34d83bcca32c468487
waterhumanB <dpfflsk45@gmail.com>
2022-11-04 16:50:33 +0900

    components layout 폴더 구조 변경

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { ReactNode } from "react";
-import Header from "./header/Header";
+import { Header } from "./index";
 import styles from "./layout.module.scss";
 
 export const siteTitle = "Next.js Sample Website";
