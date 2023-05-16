3513c1e6a358c3802083beb9db7282bb5752c394
waterhumanB <dpfflsk45@gmail.com>
2023-05-16 15:56:26 +0900

    feat: 구글,네이버,다음,빙 사이트 등록

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html } from "next/document";
+
+export default function MyDocument() {
+  return (
+    <Html lang='ko'>
+      <Head>
+        <meta
+          name='google-site-verification'
+          content='UBbrn6uYlvgqUuPSNz6397TblWqGKuSHzwf_e2jijxk'
+        />
+        <meta
+          name='naver-site-verification'
+          content='5b4676776e4bf7929cd196996b0da94331749653'
+        />
+      </Head>
+    </Html>
+  );
+}
